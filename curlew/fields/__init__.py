fixed bug with gradient constraints

curlew/fields/__init__.py
@@ -177,7 +177,8 @@ class BaseSF(LearnableBase):
                        normalize: bool = True, 
                        transform=True, 
                        return_value=False, 
-                       retain=False,
+                       retain_graph=False,
+                       create_graph=False,
                        accumulate=True) -> torch.Tensor:
         """
         Compute the gradient of the scalar potential with respect to the input coordinates. Note that this only works
@@ -193,8 +194,10 @@ class BaseSF(LearnableBase):
             If True, any defined transform function is applied before encoding and evaluating the field for `coords`.
         return_value : bool, optional
             If True, both the gradient and the scalar value at the evaluated points are returned.
-        retain : bool, optional
+        retain_graph : bool, optional
             True if the gradient graph should be retained (to allow e.g., subsequent backpropagation). Default is False.
+        create_graph : bool, optional
+            True if the gradient value should have an underlying graph to allow it to influence back-prop operations. Default is False.
         accumulate : bool, optional
             True (optional) if the gradient evaluation should contribute to the average gradient estimate. Note that this averaging
             can be reset using `self.reset_mnorm` and accessed through `self.mnorm`.
@@ -218,8 +221,8 @@ class BaseSF(LearnableBase):
             outputs=potential,
             inputs=coords,
             grad_outputs=torch.ones_like(potential),
-            create_graph=False, # should be True???
-            retain_graph=retain
+            create_graph=create_graph,
+            retain_graph=retain_graph
         )[0]
 
         # Accumulate and/or normalize gradients?
@@ -337,7 +340,6 @@ class BaseNF(BaseSF):
 
         # LOCAL LOSS FUNCTIONS
         # -----------------------------
-        
         # Value Loss
         if (C.vp is not None) and (C.vv is not None) and (isinstance(H.value_loss, str) or (H.value_loss > 0)):
             v_pred = self(C.vp, transform=transform)
@@ -347,13 +349,13 @@ class BaseNF(BaseSF):
         self.reset_mnorm() # reset accumulation
         # [ N.B. positions (and thus gradients) are in un-transformed coordinates ]
         if (C.gp is not None) and (isinstance(H.grad_loss, str) or (H.grad_loss > 0)):
-            gv_pred = self.gradient(C.gp, normalize=True, transform=transform, accumulate=True) # compute gradient direction 
+            gv_pred = self.gradient(C.gp, normalize=True, transform=transform, accumulate=True, retain_graph=True, create_graph=True) # compute gradient direction 
             L['grad_loss'] = self.vloss(gv_pred, C.gv) # N.B. constraints orientation and younging direction
 
         # Orientation loss
         # [ N.B. positions (and thus gradients) are in un-transformed coordinates ]
         if (C.gop is not None) and (isinstance(H.ori_loss, str) or (H.ori_loss > 0)):
-            gv_pred = self.gradient(C.gop, normalize=True, transform=transform, accumulate=True) # compute gradient direction 
+            gv_pred = self.gradient(C.gop, normalize=True, transform=transform, accumulate=True, retain_graph=True, create_graph=True) # compute gradient direction 
             L['ori_loss'] = torch.clamp( torch.mean( 1 - torch.abs( self.closs(gv_pred, C.gov ) ) ), min=1e-6 ) # N.B.: Orientation loss on its own fits a bit too well, numerical precision crashes avoided with the clamp - AVK
 
         # GLOBAL LOSS FUNCTIONS
@@ -373,8 +375,8 @@ class BaseNF(BaseSF):
                 ndiv = torch.zeros((gridL.shape[0]), device=curlew.device, dtype=curlew.dtype)
                 for j in range(self.input_dim):
                     # compute hessian
-                    grad_pos = self.gradient(gridL + C._offset[j], normalize=False, transform=False, accumulate=True)
-                    grad_neg = self.gradient(gridL - C._offset[j], normalize=False, transform=False, accumulate=True)
+                    grad_pos = self.gradient(gridL + C._offset[j], normalize=False, transform=False, accumulate=True, retain_graph=True, create_graph=True)
+                    grad_neg = self.gradient(gridL - C._offset[j], normalize=False, transform=False, accumulate=True, retain_graph=True, create_graph=True)
                     #for i in range(self.input_dim):
                     #    hess[:, i, j] = (grad_pos[:, i] - grad_neg[:, i])/(2*C.delta)
 
@@ -404,9 +406,9 @@ class BaseNF(BaseSF):
                 # Flatness Loss --  gradients everywhere parallel to trend
                 if (isinstance(H.flat_loss, str) or (H.flat_loss > 0)) and (C.trend is not None):
                     if transform:
-                        gv_at_grid_p = self.gradient(gridL, normalize=True, transform=self.transform) # this requires gradients relative to modern coordinates! 
+                        gv_at_grid_p = self.gradient(gridL, normalize=True, transform=self.transform, retain_graph=True, create_graph=True) # this requires gradients relative to modern coordinates! 
                     else:
-                        gv_at_grid_p = self.gradient(gridL, normalize=True, transform=False)
+                        gv_at_grid_p = self.gradient(gridL, normalize=True, transform=False, retain_graph=True, create_graph=True)
                     L['flat_loss'] = torch.mean((gv_at_grid_p - C.trend[None,:])**2) # "younging" direction
                     #flat_loss = (1 - self.closs( gv_at_grid_p, C.trend )).mean() # orientation only
 

curlew/geology/geofield.py
@@ -566,7 +566,7 @@ class GeoField( object ):
         out.grid=grid
         return out
 
-    def gradient(self, x: np.ndarray, return_vals=False, normalize=True, to_numpy=True, transform=True, retain=False):
+    def gradient(self, x: np.ndarray, return_vals=False, normalize=True, to_numpy=True, transform=True, retain_graph=False, create_graph=False):
         """
         Return the gradient vector of this GeologicalField at the specified location. Note that this
         does  not combine the results from previous scalar fields first (i.e. the prediction
@@ -585,9 +585,11 @@ class GeoField( object ):
             True if the results should be cast to a numpy array rather than a `torch.Tensor`.
         transform : bool
             True if results should be transformed into modern-day coordinates.
-        retain : bool, optional
+        retain_graph : bool, optional
             True if the gradient graph should be retained (to allow e.g., subsequent backpropagation). Default is False.
-
+        create_graph : bool, optional
+            True if the gradient value should have an underlying graph to allow it to influence back-prop operations. Default is False.
+        
         Returns
         --------
         Gradient vectors at the specified locations (`x`). If `return_vals` is `True`,
@@ -609,8 +611,8 @@ class GeoField( object ):
             outputs=pred,
             inputs=x,
             grad_outputs=torch.ones_like(pred),
-            create_graph=False, # should be True???
-            retain_graph=retain,
+            create_graph=create_graph,
+            retain_graph=retain_graph,
         )[0]
 
         # normalise gradients

curlew/geology/interactions.py
@@ -114,7 +114,7 @@ class OffsetBase(LearnableBase):
         this assumes `X` is already transformed into the local (paleo) coordinate system relevant for `G`.
         """
         # get gradient of scalar field at X and associated value
-        ds, s = G.gradient( X, normalize=normalize, return_vals=True, transform=False, to_numpy=False, retain=self.learnable() )
+        ds, s = G.gradient( X, normalize=normalize, return_vals=True, transform=False, to_numpy=False, retain_graph=self.learnable() )
         s = s.scalar
 
         # store temporary results on field so these can be later added to the output
@@ -246,7 +246,7 @@ class FaultOffset( OffsetBase ):
 
         # handle possibly learnable offset
         offset = self.offset
-        if isinstance(offset, tuple):
+        if isinstance(offset, tuple): # TODO - fix the fact that offset will never be a tuple (as if learnable it is a torch.Parameter).
             # keep offset between specified range
             offset[0].clamp(min(offset[1], offset[2]), max(offset[1], offset[2]) )
             offset = offset[0]
@@ -257,11 +257,11 @@ class FaultOffset( OffsetBase ):
             s = -s # reverse polarity to move footwall instead of hangingwall
         if isinstance(self.width, tuple):
             s1, s2, p = self.width
-            scale = (1-p)*torch.sigmoid(s*4/s1) + p*torch.sigmoid(s*4/s2)
+            scale = (1-p)*torch.sigmoid(s*4/np.clip(s1,1e-6,np.inf)) + p*torch.sigmoid(s*4/np.clip(s2, 1e-6,np.inf))
         else:
-            scale = torch.sigmoid(s*4/self.width) # N.B. -4 is approximately where the sigmoid function reaches 0
+            scale = torch.sigmoid(s*4/np.clip( self.width, 1e-6, np.inf)) # N.B. -4 is approximately where the sigmoid function reaches 0
         
-        offset = offset * scale[:, None] # scale displacements to move fault hangingwall only
+        offset = offset * scale[:, None].detach() # scale displacements to move fault hangingwall only. N.B. we disregard gradients from the scalar field here to help focus on optimising the offset term. 
 
         # apply correction for non-locally linear scalar field tangents
         # TODO - make this iterative if better approximation is needed?

curlew/synthetic.py
@@ -329,10 +329,10 @@ def anderson( shape=(1500,1000), offset1=225, offset2=250, **kwargs ):
     s0 = strati('s0', C=QuadraticField( 'f0', input_dim=2, curve=(-0.00005,0), origin=(1000,500)  ) )
     s1 = fault( 's1', 
            C=LinearField( 'f1', input_dim=2, origin=(950,550), gradient=(np.cos( np.deg2rad(35) ), np.sin( np.deg2rad(35) ))  ),
-           offset=offset1, sigma1=[0,1] ) 
+           offset=offset1, sigma1 = [0,-1] )  # extensional faults
     s2 = fault( 's2', 
            C=LinearField( 'f2', input_dim=2, origin=(1050,500), gradient=(-np.cos( np.deg2rad(35) ), np.sin( np.deg2rad(35) ))  ),
-           offset=offset2, sigma1=[0,1] )
+           offset=offset2, sigma1 = [0,-1] ) # extensional faults
     
     M = GeoModel( [s0, s1, s2], grid=G )
     s = M.predict(G)
@@ -343,7 +343,7 @@ def anderson( shape=(1500,1000), offset1=225, offset2=250, **kwargs ):
     if 'pv' in kwargs:
         del kwargs['pv']
     kwargs['pval'] = kwargs.get('pval', 1.0) # change default to sample all value constraints
-    Cf1 = sample( s1.predict(G), pv='rgb', breaks=[0.5],xstep=400, **kwargs )
+    Cf1 = sample( s1.predict(G), pv='rgb', breaks=[0.5],xstep=600, **kwargs )
     Cf2 = sample( s2.predict(G), pv='rgb', breaks=[0.5], **kwargs )
 
     C = [C[0], Cf1[0], Cf2[0], C[-1]] # combine stratigraphy, fault and property constraints

tests/test_geology.py
@@ -175,10 +175,10 @@ def test_michell():
     C, _ = michell(dims, offset=225) # create the synthetic "hutton" dataset
     C = C[:-1] # drop value constraints as they're not needed
 
-    # expand constraints for fault a bit to enable fitting
-    n = 100
-    C[1].vp = np.vstack([ C[1].vp, C[1].vp+n*C[1].gv, C[1].vp-n*C[1].gv])
-    C[1].vv = np.hstack([ C[1].vv, C[1].vv+1, C[1].vv-1])
+    # expand constraints for fault to get more value constraints
+    #n = 100
+    #C[1].vp = np.vstack([ C[1].vp, C[1].vp+n*C[1].gv, C[1].vp-n*C[1].gv])
+    #C[1].vv = np.hstack([ C[1].vv, C[1].vv+1, C[1].vv-1])
 
     from curlew import HSet
     from curlew.geology import strati, fault
@@ -211,8 +211,8 @@ def test_michell():
                 H=H, # interpolator hyperparameters
                 type=NFF,
                 sigma1=(-1,0), # horizontal stress
-                offset=(200,0,200), # Initial slip estimate, minimum slip, maximum slip
-                width=(5,100,0.5), # add a funky drag fold, coz we can and it's pretty.
+                offset=(250,0,300), # Initial slip estimate, minimum slip, maximum slip
+                width=0, # brittle fault
                 input_dim=2, # field input coordinate dimensions (2D in our case)
                 hidden_layers=[16,], # hidden layers in the multi-layer perceptron that parameterises our field
                 rff_features=32, # number of random sin and cos features to create for each scale 
@@ -223,7 +223,7 @@ def test_michell():
 
     # fit scalar fields independently [ helps get fault surface sorted out first ]
     loss1 = M.prefit( epochs=25, best=True, vb=False, early_stop=None )
-    loss2 = M.prefit( epochs=200, best=True, vb=False, early_stop=None )
+    loss2 = M.prefit( epochs=50, best=True, vb=False, early_stop=None )
 
     # check model is converging
     for k, v in loss1.items():
@@ -236,8 +236,8 @@ def test_michell():
     _, loss2 = M.fit( epochs=200, learning_rate=0.1 , early_stop=None ) # and now optimise only fault slip (and the stratigraphic field)
 
     # check model is converging
-    assert loss1['basement'][0] / loss2['basement'][0] > 1 # loss should be better
-    assert abs( s1.deformation.offset.item() - 200 ) > 10 # more than 10 m difference in offset
+    assert loss1['basement'][0] / loss2['basement'][0] > 1 # loss should be better (if only a bit)
+    assert abs( s1.deformation.offset.item() - 100 ) > 5 # more than 10 m difference in offset
 
     # check training at least runs for single-field fitting
     _, loss3 = s0.fit( 100, cache=True, faultBuffer=20)
@@ -263,12 +263,12 @@ def test_michell():
         assert abs( diff - s1.deformation.offset.item() ) < 1 # check that median offset matches offset on fault
 
     # check isolated training works with this undeformed CSet
-    _, loss3 = s0.field.fit(1, C=C0, transform=False)
-    assert loss3['basement'][0] / loss2['basement'][0] < 1.1 # loss should be similar as we didn't train much
+    _, loss4 = s0.field.fit(1, C=C0, transform=False) # Transform = False as C0 is in paleo-coordinates
+    assert loss3['basement'][0] / loss3['basement'][0] < 1.1 # loss should be similar as we didn't train much
 
     # check loss explodes if we don't transform!
-    _, loss4 = s0.field.fit(1, C=C0, transform=True)
-    assert loss4['basement'][0] / loss2['basement'][0] > 1.5
+    _, loss4 = s0.field.fit(1, C=C0, transform=True) # If Transform=True, constraints should end up in incorrect locations
+    assert loss4['basement'][0] / loss3['basement'][0] > 1.5
 
 def test_anderson():
     # load an example containing a fault
@@ -335,8 +335,8 @@ def test_anderson():
 
     # check model is converging
     assert loss1['basement'][0] > loss2['basement'][0] # loss should be better
-    assert abs( s1.field.offset.item() - (-200) ) > 1 # more than 1 m difference in offset
-    assert abs( s2.field.offset.item() - (-200) ) > 1 # more than 1 m difference in offset
+    assert abs( s1.deformation.offset.item() - (-200) ) > 1 # more than 1 m difference in offset
+    assert abs( s2.deformation.offset.item() - (-200) ) > 1 # more than 1 m difference in offset
 
 def test_anderson3D():
         # load an example containing a fault
@@ -365,6 +365,7 @@ def test_anderson3D():
     H = HSet( value_loss=1, grad_loss=1,
             mono_loss='0.1', thick_loss="1.0")
     params = dict(
+        type=NFF,
         input_dim=3, # field input coordinate dimensions
         hidden_layers=[8,], # hidden layers
         rff_features=32, # number of fourier features
@@ -372,20 +373,20 @@ def test_anderson3D():
     )
 
     s0 = strati('basement', # basement stratigraphy field
-            C3D[0], # constraints
-            H, # hyperparameters
+            C=C3D[0], # constraints
+            H=H, # hyperparameters
             **params)
     s1 = fault('fault1', # older fault field
-                C3D[1], # constraints
-                H, # hyperparameters
+                C=C3D[1], # constraints
+                H=H, # hyperparameters
                 sigma1=(0,0,1), # vertical sigma 1
                 learn_sigma=False,
                 offset=(-200,-100,-400),
                 width=1e-6,
                 **params)
     s2 = fault('fault2', # younger fault field
-                C3D[2], # constraints
-                H, # hyperparameters
+                C=C3D[2], # constraints
+                H=H, # hyperparameters
                 sigma1=(0,0,1), # vertical sigma 1
                 learn_sigma=False,
                 offset=(-200,-100,-400),
@@ -406,8 +407,8 @@ def test_anderson3D():
 
     # check model is converging
     assert loss1['basement'][0] > loss2['basement'][0] # loss should be better
-    assert abs( s1.field.offset.item() - (-200) ) > 1 # more than 1 m difference in offset
-    assert abs( s2.field.offset.item() - (-200) ) > 1 # more than 1 m difference in offset
+    assert abs( s1.deformation.offset.item() - (-200) ) > 1 # more than 1 m difference in offset
+    assert abs( s2.deformation.offset.item() - (-200) ) > 1 # more than 1 m difference in offset
 
     tcont = False # test contouring (requires skimage)
     try: